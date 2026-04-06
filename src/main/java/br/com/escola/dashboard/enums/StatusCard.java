c391f42aae8e2dfae4918e49300e0c9e6e4f5c24
Kelvin Rosendo <rosendokelvin8@gmail.com>
2026-04-06 18:08:46 -0300

    feat: adiciona estilos e layout responsivo para cards e botões

---
+++ src/main/java/br/com/escola/dashboard/enums/StatusCard.java
@@ -0,0 +1,16 @@
+package br.com.escola.dashboard.enums;
+
+/**
+ * 🔹 StatusCard
+ *
+ * Esse enum define os possíveis status de um card/evento no sistema.
+ *
+ * PENDENTE      -> ainda não começou
+ * EM_ANDAMENTO -> está sendo realizado / preparado
+ * CONCLUIDO    -> já foi finalizado
+ */
+public enum StatusCard {
+    PENDENTE,
+    EM_ANDAMENTO,
+    CONCLUIDO
+}
